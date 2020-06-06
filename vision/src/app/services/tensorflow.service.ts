e2539a6cc114ad0ee8caab74ee336a96b4e7f453
ed99win <edwin99@live.ca>
2020-06-06 15:11:27 -0400

    not loading weights

--- vision/src/app/services/tensorflow.service.ts
+++ vision/src/app/services/tensorflow.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import * as tf from '@tensorflow/tfjs'
+import { strict } from 'assert';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,14 @@ import * as tf from '@tensorflow/tfjs'
 export class TensorflowService {
   private Model: tf.LayersModel
 
-  // constructor() {
-  //   // tf.loadLayersModel(environment.backend.Tfmodel)
-  //   // .then((layerModel) => {
-  //   //   console.log('model imported successfully')
-  //   //   this.Model = layerModel
-  //   // })
-  //   // .catch((err) => {
-  //   //   console.log(err)
-  //   // })
-  // }
+  constructor() {
+    tf.loadLayersModel(environment.backend.Tfmodel, {strict:false})
+    .then((layerModel) => {
+      console.log('model imported successfully')
+      this.Model = layerModel
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
 }
