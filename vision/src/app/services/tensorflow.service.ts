1a918b1c2a3cca247e5623942e757cfd2b35919b
ed99win <edwin99@live.ca>
2020-06-05 21:47:02 -0400

    added tensorflow service

---
+++ vision/src/app/services/tensorflow.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import * as tf from '@tensorflow/tfjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TensorflowService {
+
+  constructor() {
+    
+  }
+}
