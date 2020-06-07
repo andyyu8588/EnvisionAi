e79c31a906d23a5bcf3422dd358661d8f8af35a0
MoistWhale <andy.yu8588@gmail.com>
2020-06-06 21:36:08 -0400

    started map sync

--- vision/src/app/services/map.service.ts
+++ vision/src/app/services/map.service.ts
@@ -7,18 +7,9 @@ import * as Mapboxgl from 'mapbox-gl'
   providedIn: 'root'
 })
 export class MapService {
-  map: Mapboxgl.Map
+  
   constructor() {
 
   }
 
-  buildMap() {
-    this.map = new Mapboxgl.Map({
-      accessToken: environment.mapbox.token,
-      container: 'mapbox', // container id
-      style: 'mapbox://styles/travelnet/ckb32151e0ge11ijopc5mbegm',
-      center: [-71.22, 46.85], // starting position
-      zoom: 2, // starting zoom
-    })
-  }
 }
