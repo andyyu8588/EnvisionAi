bba091e1e5e1ba5136925f0bf95b9abf915e32c5
ed99win <edwin99@live.ca>
2020-06-16 16:41:04 -0400

    ok

--- backend/routes/download.js
+++ backend/routes/download.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/', (req, res, next) => {
     if (req) {
-        res.setHeader('Content-Disposition', 'attachement; filename=download.rar')
+        res.setHeader('Content-Disposition', 'attachement')
         res.status(200).sendFile('app.rar', {root: path.resolve(__dirname, '..')})
     } else {
         res.status(404)
